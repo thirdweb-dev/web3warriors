a4868ddc01f5f148ed1b7e76f286afad1682a261
Nacho Iacovino <nacho.iacovino@gmail.com>
2023-05-18 02:29:10 +0200

    Addressed feedback

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/components/Footer';
 import './globals.css';
 import { Inter, Oswald, Rye } from 'next/font/google';
 
@@ -53,17 +54,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${oswald.variable} ${rye.variable}`}>
         {children}
-        <div>
-          <div className="container gap-12 px-4 pt-8 pb-16 mx-auto sm:px-6 lg:px-8 ">
-            <div className="lg:mx-auto lg:max-w-5xl">
-              <h4 className="text-lg text-center uppercase lg:text-xl font-rye">Web3 Warriors</h4>
-              <div className="w-full h-0.5 bg-gray-900 my-4" />
-              <div className="flex flex-col lg:flex-row lg:justify-between">
-                <span className="text-sm text-center text-gray-600 lg:text-left">All Rights Reserved @thirdweb</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Footer />
       </body>
     </html>
   );
--- components/HeroTitle.tsx
+++ components/HeroTitle.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const HeroTitle = () => {
   return (
     <div>
-      <h3 className="text-sm lg:text-xl font-oswald">thirdweb presents</h3>
+      <h3 className="text-lg lg:text-xl font-oswald">thirdweb presents</h3>
       <h1 className="mt-2 text-4xl uppercase lg:text-5xl font-rye">Web3 Warriors</h1>
       <span className="flex items-center justify-center w-full gap-2.5 mt-3">
         <h2 className="text-xl leading-7 font-oswald">Built on</h2>
